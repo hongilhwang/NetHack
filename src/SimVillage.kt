dcae511571ff8d73a8ab3fb1349d6f64095440ca
hongil hwang <hongilhwang@gmail.com>
2020-06-07 13:47:25 +0900

    람다는 편하지고, 가독성이 좋지만, jvm 에서는 이를 객체로 여기고, 모든 변수에 메모리를 할당하고 메모리가 많이 소비된다. 이런점을 고치기 위해, 코틀린에서는 성능 향샹을 위해 inline 키워드를 제공한다.
    더불어 함수참조에 대해서도 확인한다.

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,19 +1,23 @@
 
 fun main(args : Array<String>){
+    runSimulation()
+}
 
-    runSimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
-        val currentYear: Int = 2019
-        println("$numBuildings 채의 건물이 추가됨");
-        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("김선달"))
+}
+fun configureGreetingFunction(): (String) -> String{
+    val structureType = "병원"
+    var numBuildings = 5
+    return {
+        val currentYear = 2019
+        numBuildings += 1
+        println("$numBuildings 채의 $structureType 이 추가됨")
+        "SimVillage 방문을 환영합니다, $it! (copyright $currentYear)"
     }
-
 }
 
-inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int)-> String ){
-    val numBuildings = (1..3).shuffled().last() // 1, 2, 3 중 하나를 무작위로 선택한다.
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
 
 fun printConstructionCost( numBuildings: Int ){
     val cost = 500;
