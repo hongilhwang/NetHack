e6db6dcf65017f344dc74ceb2612fafdee659e2c
hongil hwang <hongilhwang@gmail.com>
2020-07-06 09:57:04 +0900

    문자열 다루기

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,7 +1,32 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main(args: Array<String>){
 
-    var beverage = readLine()?.capitalize()?: "맥주"
+    placeOrder("shandy,Dragon's Breath,5.91");
+}
 
-    // beverage = null;
-    println(beverage);
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")){
+        when(it.value){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        } }
+
+private fun placeOrder(menuData: String){
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+    val (type, name, price) = menuData.split(',')
+
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+    val phrase = "와, $name 진짜 좋구나!"
+    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+
 }
