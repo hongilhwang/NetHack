f6c41eb4b22df51412d3175fdf4f1ddb4ffb9d1f
hongil hwang <hongilhwang@gmail.com>
2020-07-06 22:40:20 +0900

    문자열 다루기

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,5 +1,6 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 
+
 fun main(args: Array<String>){
 
     placeOrder("shandy,Dragon's Breath,5.91");
@@ -26,7 +27,12 @@ private fun placeOrder(menuData: String){
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
 
-    val phrase = "와, $name 진짜 좋구나!"
-    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+    val phrase = if(name == "Dragon's Breath"){
+        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+    }else{
+        "마드리갈이 말한다: 감사합니다 $name"
+    }
+
+    println(phrase)
 
 }
