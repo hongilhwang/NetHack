8a7604fcc84e4b2b101618d8c218be5e4bc182af
hongil hwang <hongilhwang@gmail.com>
2020-07-08 00:12:45 +0900

    list 사용

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -4,9 +4,12 @@ const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
+val patronList = listOf("Eli", "Mordoc", "Sophie")
 
 fun main(args: Array<String>){
-    placeOrder("shandy,Dragon's Breath,5.91");
+    placeOrder("shandy,Dragon's Breath,5.91")
+
+    println(patronList)
 }
 
 fun performPurchase(price: Double){
